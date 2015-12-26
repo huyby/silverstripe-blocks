9e2e33d1c0aa96f1b5e09d6060853e4a7e82f6cf
Al Twohill <al@twohill.nz>
2015-12-27 11:16:42 +1300

    Updated summary blocks to use linkable

--- code/dataobjects/BootStrapSummaryBlock.php
+++ code/dataobjects/BootStrapSummaryBlock.php
@@ -7,12 +7,14 @@ class BootStrapSummaryBlock extends BootStrapBlock {
 	private static $singular_name = 'Summary Block';
 	private static $plural_name = 'Summary Blocks';
 
-	private static $db = array(
-		'Link' => 'Varchar(255)',
-		'OpenInNewWindow' => 'Boolean',
-	);
-
 	private static $has_one = array(
+		'Link' => 'Link',
 		'Image' => 'Image',
 	);
+
+	public function getCMSFields() {
+		$fields = parent::getCMSFields();
+		$fields->replaceField('LinkID', LinkField::create('LinkID', 'Link'));
+		return $fields;
+	}
 }
--- templates/Includes/BootStrapSummaryBlock.ss
+++ templates/Includes/BootStrapSummaryBlock.ss
@@ -1,3 +1,3 @@
-<% if $ShowTitle %><h2><a href="$Link" <% if $OpenInNewWindow %>target="_blank"<% end_if %>>$Title</a></h2><% end_if %>
-<% if $Image %><a href="$Link">$Image</a><% end_if %>
+<% if $ShowTitle %><h2><a href="$Link.LinkURL" $Link.TargetAttr >$Title</a></h2><% end_if %>
+<% if $Image %><<a href="$Link.LinkURL" $Link.TargetAttr >$Image</a><% end_if %>
 $Content
